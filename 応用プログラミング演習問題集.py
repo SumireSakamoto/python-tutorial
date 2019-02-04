130d8b464c4a6feefba56edf31899f81dce271a4
sumire <violet18ri1003@gmail.com>
2019-02-04 19:43:46 +0900

    4章も解き終わった

--- 応用プログラミング演習問題集.py
+++ 応用プログラミング演習問題集.py
@@ -69,19 +69,16 @@ print(x)
 
 # 第3章
 # 3-1(1)
-print('整数を入れてください')
-x = int(input())
+x = int(input('整数を入れてください'))
 print(x)
 
 # 3-1(2)(3)はとばす
 # 3-1(4)
-print('seisuu = ?')
-x = int(input())
+x = int(input('seisuu = ?'))
 print(x)
 
 # 3-1(5)
-print('小数を入れてください')
-x = float(input())
+x = float(input('小数を入れてください'))
 print(x)
 
 # 3-2(1)
@@ -110,23 +107,19 @@ y = int(input())
 print(x*y)
 
 # 3-3 -1
-print('借金額を入力してください')
-shakkin = int(input())
-print('月の返済額を入力してください')
-hensai = int(input())
+shakkin = int(input('借金額を入力してください'))
+hensai = int(input('月の返済額を入力してください'))
 year = (shakkin / hensai)//12
 month = (shakkin / hensai) % 12
 print('返済にかかる年月は', year, '年', month, 'ヶ月です。')
 
 # 3-3 -2
-print('毎年のボーナスから返済したい額を入力してください')
-bonus = int(input())
+bonus = int(input('毎年のボーナスから返済したい額を入力してください'))
 year2 = ((shakkin-bonus)/hensai)/12
 print('ボーナスから', bonus, '円返済すると、', year-year2, '年返済が早まります。')
 
 # 3-3 -3
-print('返済を完了したい年数を入力してください。')
-nensu = int(input())
+nensu = int(input('返済を完了したい年数を入力してください。'))
 bonus2 = shakkin - (nensu * hensai)
 print(nensu, '年で返済を完了するにはボーナスで', bonus2, '円返済してください')
 
@@ -167,4 +160,129 @@ y = int(input())
 print('和は',x+y)
 print('差は',abs(x-y))
 print('積は',x*y)
+
+# 第4章
+# 例4-1
+if 0<10:
+    print 'OK'
+
+# 例4-2:
+x = input('値を入れてください。ある数字と等しいか判定します')
+if x+25000 == 3500*9:
+    print('OK')
+else:
+    print('Booo')
+
+# 例4-3
+x = input('値を入れてください')
+if x > 0:
+    print('positive')
+    if x > 100:
+        print('100ijou')
+    else:
+        print('100miman')
+elif x == 0:
+    print('zero')
+else:
+    print('negative')
+
+# 4-1 (1)
+x = input('値を入れてください')
+if x == 0:
+    print('zero')
+
+# 4-1 (2)
+x = input('値を入れてください')
+if x >= 100:
+    print('OK')
+
+# 4-1 (3)
+x = input('値を入れてください')
+if x > 0:
+    print('positive')
+else:
+    print('negative')
+
+# 4-1 (4)
+x = input('値を入れてください')
+y = input('値を入れてください')
+if x == y:
+    print('equal')
+
+# 4-2 (1)
+x = input('値を入れてください')
+if x == 10:
+    print('x=10')
+elif x > 10:
+    print('x>10')
+else:
+    print('x<10')
+
+# 4-2 (2)
+x = input('値を入れてください')
+if x == 1:
+    print('x=1')
+elif x == 2:
+    print('x=2')
+else:
+    print('etc')
+
+# 4-2 (3)
+x = input('値を入れてください')
+if x < 0:
+    if x == 0:
+        print('zero')
+    else:
+        print('negative')
+elif x > 0:
+    print('positive')
+    if x >= 100:
+        print('100ijou')
+    else:
+        print('100miman')
+
+# 4-3
+print('値を2つ入力してください。')
+x = input('一つ目')
+y = input('二つ目')
+if x >= y:
+    print(x-y)
+else:
+    print('マイナスになります')
+
+# 4-4
+x = input('1~3の値を入れてください')
+if x == 1:
+    print('(。-∀-)')
+elif x == 2:
+    print('( *´艸｀)')
+else:
+    print('(*^^)v')
+
+# 4-5
+x = int(input('整数を入力してください'))
+if x % 2 == 0:
+    print('Guusuu')
+else:
+    print('Kisuu')
+
+# 4-6
+x = input('値を入れてください')
+if 10 <= x <= 20:
+    print('ok')
+
+# 4-7
+x = input('値を入れてください')
+if 10 <= x <= 20:
+    print('(^_-)-☆')
+elif 50 <= x <= 60:
+    print('(/・ω・)/')
+
+# 4-8
+x = input('値を入れてください')
+if x > 0:
+    print(x)
+else:
+    y = input('値を入れてください')
+    print(x*y)
 
